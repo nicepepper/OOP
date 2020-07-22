95fcd5c71d100bd16fd97de05a7838907e0b2466
Rashitov Rishat <wildrage88@gmail.com>
2020-07-22 15:22:48 +0300

    Update GeometricFigures: removed default constructor ICanvas

--- lab4/GeometricFigures/GeometricFigures/Canvas.h
+++ lab4/GeometricFigures/GeometricFigures/Canvas.h
@@ -5,8 +5,6 @@
 class ICanvas
 {
 public:
-	ICanvas() = default;
-
 	virtual void DrawLine(const CPoint& from, const CPoint& to, uint32_t lineColor) = 0;
 	virtual void FillPolygon(const std::vector<CPoint>& points, uint32_t fillColor) = 0;
 	virtual void DrawCircle(const CPoint& center, float radius, uint32_t lineColor) = 0;
